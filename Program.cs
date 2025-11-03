f388e205284cf912f375ef3182f0c141fbf9c51b
yuko <rienokpunaise@gmail.com>
2025-11-03 11:17:27 +0100

    Started the main file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 ﻿
+using System.Net.Sockets;
+
 namespace BedSharp;
 
 class Program
 {
     public void Main(string[] args)
     {
+        int port = 19132;
+        
+        UdpClient client = new UdpClient(port);
+        
         
     }
 }

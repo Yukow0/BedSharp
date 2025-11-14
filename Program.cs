3efc22b074c0eb7c91283e8fbc7d7af4b376b37c
yuko <rienokpunaise@gmail.com>
2025-11-14 11:24:19 +0100

    Added two files with every packet of bedrock protocol ( BedrockProtocolInfo.cs) and Raknet protocol ( RakNetProtocolInfo.cs) and modified in consequence.

--- Program.cs
+++ Program.cs
@@ -12,17 +12,21 @@ class Program
         int port = 19132;
         Random rnd = new Random();
         ulong SERVER_ID = ((ulong)rnd.Next() << 32) | (uint)rnd.Next();
-        const short MTU_SIZE = 1492;
         
+
         Console.WriteLine($"Server ID: {SERVER_ID}");
-        string serverData = $"MCPE;Serveur C# de Rieno;859;1.21.120;0;20;{SERVER_ID};BedSharp;Survival;1;19132;19133;";
-        
-        byte[] MAGIC = 
-        {
-            0x00, 0xFF, 0xFF, 0x00, 0xFE, 0xFE, 0xFE, 0xFE,
-            0xFD, 0xFD, 0xFD, 0xFD, 0x12, 0x34, 0x56, 0x78
+        string serverData = $"MCPE;Serveur C# de Rieno;859;1.21.122;0;20;{SERVER_ID};BedSharp;Survival;1;19132;19133;";
+
+        byte[] MAGIC = new byte[] 
+        { 
+        0x00, 0xFF, 0xFF, 0x00, 0xFE, 0xFE, 0xFE, 0xFE, 
+        0xFD, 0xFD, 0xFD, 0xFD, 0x12, 0x34, 0x56, 0x78 
         };
+
+        int magic;
+
         
+
         UdpClient listener = new UdpClient(port);
 
         Console.WriteLine($"Listening on port {port}..");
@@ -36,36 +40,168 @@ class Program
                 byte[] datagram = listener.Receive(ref clientEndPoint);
 
                 Console.WriteLine($"\n========================================");
-                Console.WriteLine($"Received {datagram.Length} bytes from {clientEndPoint.Address}:{clientEndPoint.Port}");
+                Console.WriteLine(
+                    $"Received {datagram.Length} bytes from {clientEndPoint.Address}:{clientEndPoint.Port}");
                 Console.WriteLine($"Packet ID: 0x{datagram[0]:X2}");
                 Console.WriteLine($"Full packet: {BitConverter.ToString(datagram)}");
                 Console.WriteLine($"========================================");
 
-                if (datagram[0] == 0x01 && datagram.Length > 0)
+                using (MemoryStream msRead = new MemoryStream(datagram))
+                using (BinaryReader br = new BinaryReader(msRead))
                 {
-                    using (MemoryStream ms = new MemoryStream(datagram))
-                    using (BinaryWriter bw = new BinaryWriter(ms))
-                    using (BinaryReader br = new BinaryReader(ms))
+                    if (datagram[0] == (byte)MessageIdentifiers.IdUnconnectedPing)
                     {
-                        br.ReadByte(); // Skip the packet ID
-                        
-                        //Read the timestamp
+                        br.ReadByte(); // Skip the packet ID (0x01)
+
+                        // Read the timestamp (Big Endian)
                         long clientTime = BinaryPrimitives.ReadInt64BigEndian(br.ReadBytes(8));
+
                         
-                        //Read and verify the magic
+
+                        // Construct response packet (0x1c - Unconnected Pong)
+                        using (MemoryStream msWrite = new MemoryStream())
+                        using (BinaryWriter bw = new BinaryWriter(msWrite))
+                        {
+                            // Packet ID
+                            bw.Write((byte)MessageIdentifiers.IdUnconnectedPong);
+
+                            // Client timestamp (Big Endian)
+                            byte[] timeBytes = new byte[8];
+                            BinaryPrimitives.WriteInt64BigEndian(timeBytes, clientTime);
+                            bw.Write(timeBytes);
+
+                            // Server GUID (Big Endian)
+                            byte[] guidBytes = new byte[8];
+                            BinaryPrimitives.WriteInt64BigEndian(guidBytes, (long)SERVER_ID);
+                            bw.Write(guidBytes);
+
+                            // Magic
+                            bw.Write(MAGIC);
+
+                            // String length (Big Endian, 2 bytes = ushort)
+                            byte[] serverDataBytes = Encoding.UTF8.GetBytes(serverData);
+                            byte[] lengthBytes = new byte[2];
+                            BinaryPrimitives.WriteUInt16BigEndian(lengthBytes, (ushort)serverDataBytes.Length);
+                            bw.Write(lengthBytes);
+
+                            // Server data string
+                            bw.Write(serverDataBytes);
+
+                            // Send the response
+                            byte[] responsePacket = msWrite.ToArray();
+                            
+                            listener.Send(responsePacket, responsePacket.Length, clientEndPoint);
+                            Console.WriteLine($"Response packet ({responsePacket.Length} bytes): {BitConverter.ToString(responsePacket)}");
+                            Console.WriteLine($"Server data: {serverData}");
+                            Console.WriteLine($"Server data length: {serverDataBytes.Length}");
+                            Console.WriteLine(
+                                $"Sent Unconnected Pong to {clientEndPoint.Address}:{clientEndPoint.Port}");
+                        }
+                    }
+                    else if (datagram[0] == (byte)MessageIdentifiers.IdOpenConnectionRequest1 && datagram.Length > 0)
+                    {
+                        Console.WriteLine(">>> Received Open Connection Request 1!");
+    
+                        br.ReadByte(); // Skip packet ID (0x05)
                         byte[] receivedMagic = br.ReadBytes(16);
-                        bool magicValid = receivedMagic.SequenceEqual(MAGIC);
+                        byte protocolVersion = br.ReadByte();
+    
+                        Console.WriteLine($"Protocol: {protocolVersion}, Magic Valid: {receivedMagic.SequenceEqual(MAGIC)}");
+                        Console.WriteLine($"Client MTU: {datagram.Length}");
+    
+                        // Construct the answer (0x06 - Open Connection Reply 1)
+                        using (MemoryStream msWrite = new MemoryStream())
+                        using (BinaryWriter bw = new BinaryWriter(msWrite))
+                        {
+                            // Packet ID (0x06)
+                            bw.Write((byte)MessageIdentifiers.IdOpenConnectionReply1);
+        
+                            // Magic (16 bytes)
+                            bw.Write(MAGIC);
+        
+                            // Server GUID (Big Endian, 8 bytes)
+                            byte[] guidBytes = new byte[8];
+                            BinaryPrimitives.WriteInt64BigEndian(guidBytes, (long)SERVER_ID);
+                            bw.Write(guidBytes);
+        
+                            // Use Security (1 byte) - 0x00 = false
+                            bw.Write((byte)0x00);
+        
+                            // MTU Size (Big Endian, 2 bytes)
+                            ushort mtuSize = (ushort)(datagram.Length); // Use the MTU of the client
+                            byte[] mtuBytes = new byte[2];
+                            BinaryPrimitives.WriteUInt16BigEndian(mtuBytes, mtuSize);
+                            bw.Write(mtuBytes);
+        
+                            // Send the packet
+                            byte[] responsePacket = msWrite.ToArray();
+                            listener.Send(responsePacket, responsePacket.Length, clientEndPoint);
+        
+                            Console.WriteLine($"Sent Open Connection Reply 1 ({responsePacket.Length} bytes)");
+                            Console.WriteLine($"Response: {BitConverter.ToString(responsePacket)}");
+                        }
+                    }
+                    else if (datagram[0] == (byte)MessageIdentifiers.IdOpenConnectionRequest2 && datagram.Length > 0)
+                    {
+                        Console.WriteLine(">>> Received Open Connection Request 2!");
 
-                        Console.WriteLine($"Client Time: {clientTime}");
-                        //Packet ID
-                        bw.Write((byte)0x1c);
-                        
-                        //TimeStamp of the client
-                        byte[] timeBytes = BitConverter.GetBytes(clientTime);
+                        br.ReadByte(); // Skip packet ID (0x07)
 
+                        // Read MTU
+                        ushort mtuSize = BinaryPrimitives.ReadUInt16BigEndian(br.ReadBytes(2));
+
+                        // Read client GUID
+                        long clientGuid = BinaryPrimitives.ReadInt64BigEndian(br.ReadBytes(8));
+
+                        Console.WriteLine($"MTU: {mtuSize}, Client GUID: {clientGuid}");
+
+                        // Build response (0x08 - Open Connection Reply 2)
+                        using (MemoryStream msWrite = new MemoryStream())
+                        using (BinaryWriter bw = new BinaryWriter(msWrite))
+                        {
+                            // Packet ID (0x08)
+                            bw.Write((byte)MessageIdentifiers.IdOpenConnectionReply2);
+
+                            // Magic (16 bytes)
+                            bw.Write(MAGIC);
+
+                            // Server GUID (Big Endian, 8 bytes)
+                            byte[] guidBytes = new byte[8];
+                            BinaryPrimitives.WriteInt64BigEndian(guidBytes, (long)SERVER_ID);
+                            bw.Write(guidBytes);
+
+                            // Client Address (Version + IP + Port)
+                            bw.Write((byte)4); // IPv4
+
+                            // IP Address (4 bytes, inverted with ~)
+                            byte[] ipBytes = clientEndPoint.Address.GetAddressBytes();
+                            foreach (byte b in ipBytes)
+                            {
+                                bw.Write((byte)~b);
+                            }
+
+                            // Port (Big Endian, 2 bytes)
+                            byte[] portBytes = new byte[2];
+                            BinaryPrimitives.WriteUInt16BigEndian(portBytes, (ushort)clientEndPoint.Port);
+                            bw.Write(portBytes);
+
+                            // MTU Size (Big Endian, 2 bytes)
+                            byte[] mtuBytes = new byte[2];
+                            BinaryPrimitives.WriteUInt16BigEndian(mtuBytes, mtuSize);
+                            bw.Write(mtuBytes);
+
+                            // Security (1 byte) - 0x00 = false
+                            bw.Write((byte)0x00);
+
+                            // Send packet
+                            byte[] responsePacket = msWrite.ToArray();
+                            listener.Send(responsePacket, responsePacket.Length, clientEndPoint);
+
+                            Console.WriteLine($"Sent Open Connection Reply 2 ({responsePacket.Length} bytes)");
+                            Console.WriteLine($"Response: {BitConverter.ToString(responsePacket)}");
+                        }
                     }
                 }
-                   
             }
         }
         catch (Exception e)
@@ -78,5 +214,4 @@ class Program
             listener.Close();
         }
     }
-    
 }
---
+++ RakNetProtocolInfo.cs
@@ -0,0 +1,168 @@
+﻿namespace BedSharp;
+
+public enum MessageIdentifiers : byte
+{
+    IdConnectedPing = 0x00,
+    IdUnconnectedPing = 0x01,
+    IdUnconnectedPingOpenConnections = 0x02,
+    IdConnectedPong = 0x03,
+    IdDetectLostConnections = 0x04,
+    IdOpenConnectionRequest1 = 0x05,
+    IdOpenConnectionReply1 = 0x06,
+    IdOpenConnectionRequest2 = 0x07,
+    IdOpenConnectionReply2 = 0x08,
+    IdConnectionRequest = 0x09,
+    IdRemoteSystemRequiresPublicKey = 0x0A,
+    IdOurSystemRequiresSecurity = 0x0B,
+    IdPublicKeyMismatch = 0x0C,
+    IdOutOfBandInternal = 0x0D,
+    IdSndReceiptAcked = 0x0E,
+    IdSndReceiptLoss = 0x0F,
+
+    IdConnectionRequestAccepted = 0x10,
+    IdConnectionAttemptFailed = 0x11,
+    IdAlreadyConnected = 0x12,
+    IdNewIncomingConnection = 0x13,
+    IdNoFreeIncomingConnections = 0x14,
+    IdDisconnectionNotification = 0x15,
+    IdConnectionLost = 0x16,
+    IdConnectionBanned = 0x17,
+    IdInvalidPassword = 0x18,
+    IdIncompatibleProtocolVersion = 0x19,
+    IdIpRecentlyConnected = 0x1A,
+    IdTimestamp = 0x1B,
+    IdUnconnectedPong = 0x1C,
+    IdAdvertiseSystem = 0x1D,
+    IdDownloadProgress = 0x1E,
+
+    IdRemoteDisconnectionNotification = 0x1F,
+    IdRemoteConnectionLost = 0x20,
+    IdRemoteNewIncomingConnection = 0x21,
+
+    IdFileListTransferHeader = 0x22,
+    IdFileListTransferFile = 0x23,
+    IdFileListReferencePushAck = 0x24,
+
+    IdDdtDownloadRequest = 0x25,
+
+    IdTransportString = 0x26,
+
+    IdReplicaManagerConstruction = 0x27,
+    IdReplicaManagerScopeChange = 0x28,
+    IdReplicaManagerSerialize = 0x29,
+    IdReplicaManagerDownloadStarted = 0x2A,
+    IdReplicaManagerDownloadComplete = 0x2B,
+
+    IdRakvoiceOpenChannelRequest = 0x2C,
+    IdRakvoiceOpenChannelReply = 0x2D,
+    IdRakvoiceCloseChannel = 0x2E,
+    IdRakvoiceData = 0x2F,
+
+    IdAutopatcherGetChangelistSinceDate = 0x30,
+    IdAutopatcherCreationList = 0x31,
+    IdAutopatcherDeletionList = 0x32,
+    IdAutopatcherGetPatch = 0x33,
+    IdAutopatcherPatchList = 0x34,
+    IdAutopatcherRepositoryFatalError = 0x35,
+    IdAutopatcherCannotDownloadOriginalUnmodifiedFiles = 0x36,
+    IdAutopatcherFinishedInternal = 0x37,
+    IdAutopatcherFinished = 0x38,
+    IdAutopatcherRestartApplication = 0x39,
+
+    IdNatPunchthroughRequest = 0x3A,
+    IdNatConnectAtTime = 0x3B,
+    IdNatGetMostRecentPort = 0x3C,
+    IdNatClientReady = 0x3D,
+
+    IdNatTargetNotConnected = 0x3E,
+    IdNatTargetUnresponsive = 0x3F,
+    IdNatConnectionToTargetLost = 0x40,
+    IdNatAlreadyInProgress = 0x41,
+    IdNatPunchthroughFailed = 0x42,
+    IdNatPunchthroughSucceeded = 0x43,
+
+    IdReadyEventSet = 0x44,
+    IdReadyEventUnset = 0x45,
+    IdReadyEventAllSet = 0x46,
+    IdReadyEventQuery = 0x47,
+
+    IdLobbyGeneral = 0x48,
+
+    IdRpcRemoteError = 0x49,
+    IdRpcPlugin = 0x4A,
+
+    IdFileListReferencePush = 0x4B,
+    IdReadyEventForceAllSet = 0x4C,
+
+    IdRoomsExecuteFunc = 0x4D,
+    IdRoomsLogonStatus = 0x4E,
+    IdRoomsHandleChange = 0x4F,
+
+    IdLobby2SendMessage = 0x50,
+    IdLobby2ServerError = 0x51,
+
+    IdFcm2NewHost = 0x52,
+    IdFcm2RequestFcmguid = 0x53,
+    IdFcm2RespondConnectionCount = 0x54,
+    IdFcm2InformFcmguid = 0x55,
+    IdFcm2UpdateMinTotalConnectionCount = 0x56,
+    IdFcm2VerifiedJoinStart = 0x57,
+    IdFcm2VerifiedJoinCapable = 0x58,
+    IdFcm2VerifiedJoinFailed = 0x59,
+    IdFcm2VerifiedJoinAccepted = 0x5A,
+    IdFcm2VerifiedJoinRejected = 0x5B,
+
+    IdUdpProxyGeneral = 0x5C,
+
+    IdSqLite3Exec = 0x5D,
+    IdSqLite3UnknownDb = 0x5E,
+    IdSqlLiteLogger = 0x5F,
+
+    IdNatTypeDetectionRequest = 0x60,
+    IdNatTypeDetectionResult = 0x61,
+
+    IdRouter2Internal = 0x62,
+    IdRouter2ForwardingNoPath = 0x63,
+    IdRouter2ForwardingEstablished = 0x64,
+    IdRouter2Rerouted = 0x65,
+
+    IdTeamBalancerInternal = 0x66,
+    IdTeamBalancerRequestedTeamFull = 0x67,
+    IdTeamBalancerRequestedTeamLocked = 0x68,
+    IdTeamBalancerTeamRequestedCancelled = 0x69,
+    IdTeamBalancerTeamAssigned = 0x6A,
+
+    IdLightspeedIntegration = 0x6B,
+    IdXboxLobby = 0x6C,
+
+    IdTwoWayAuthenticationIncomingChallengeSuccess = 0x6D,
+    IdTwoWayAuthenticationOutgoingChallengeSuccess = 0x6E,
+    IdTwoWayAuthenticationIncomingChallengeFailure = 0x6F,
+    IdTwoWayAuthenticationOutgoingChallengeFailure = 0x70,
+    IdTwoWayAuthenticationOutgoingChallengeTimeout = 0x71,
+    IdTwoWayAuthenticationNegotiation = 0x72,
+
+    IdCloudPostRequest = 0x73,
+    IdCloudReleaseRequest = 0x74,
+    IdCloudGetRequest = 0x75,
+    IdCloudGetResponse = 0x76,
+    IdCloudUnsubscribeRequest = 0x77,
+    IdCloudServerToServerCommand = 0x78,
+    IdCloudSubscriptionNotification = 0x79,
+
+    IdLibVoice = 0x7A,
+
+    IdRelayPlugin = 0x7B,
+    IdNatRequestBoundAddresses = 0x7C,
+    IdNatRespondBoundAddresses = 0x7D,
+    IdFcm2UpdateUserContext = 0x7E,
+    IdReserved3 = 0x7F,
+    IdReserved4 = 0x80,
+    IdReserved5 = 0x81,
+    IdReserved6 = 0x82,
+    IdReserved7 = 0x83,
+    IdReserved8 = 0x84,
+    IdReserved9 = 0x85,
+
+    IdUserPacketEnum = 0x86
+}
